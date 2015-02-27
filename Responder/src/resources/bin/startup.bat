6fba34d0384e4a3dd3b9cd27e2c53510701ca29c
Jeff Simpson <jeff.simpson@volant-associates.com>
2015-02-27 15:04:08 -0500

    Setup the resources in a zip structure. Edited the resource files to use the ARGO_HOME environment variable. Added logging support for the Responder.

---
+++ Responder/src/resources/bin/startup.bat
@@ -0,0 +1,1 @@
+java -cp %ARGO_HOME%/lib/Responder-0.2.1-SNAPSHOT.jar -Dnet.java.preferIPv4Stack=true ws.argo.Responder.Responder -pf %ARGO_HOME%/config/responderConfig.prop
--- Responder/src/resources/startup.bat
+++
@@ -1,1 +0,0 @@
-java -cp Responser-0.2.1-SNAPSHOT.jar -Dnet.java.preferIPv4Stack=true ws.argo.Responder.Responder -pf c:/argo/responderConfig.prop
