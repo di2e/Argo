837379b166c774311ff7bd026b3f44b88b550aaf
Jeff Simpson <jeff.simpson@volant-associates.com>
2015-03-23 21:19:02 -0400

    Added open source headers and copyright information.  Further prep for clean open source release

--- Responder/src/main/java/ws/argo/Responder/plugin/jmdns/MulticastDNSProbeHandlerPluginImpl.java
+++ Responder/src/main/java/ws/argo/Responder/plugin/jmdns/MulticastDNSProbeHandlerPluginImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 Jeff Simpson.
+ *
+ * Licensed under the MIT License, (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ws.argo.Responder.plugin.jmdns;
 
 import java.io.IOException;
@@ -6,12 +21,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.jmdns.JmDNS;
-import javax.jmdns.JmmDNS;
 import javax.jmdns.ServiceEvent;
 import javax.jmdns.ServiceListener;
 import javax.jmdns.ServiceTypeListener;
-
-import org.w3c.dom.Element;
 
 import ws.argo.Responder.ProbePayloadBean;
 import ws.argo.Responder.ResponsePayloadBean;
