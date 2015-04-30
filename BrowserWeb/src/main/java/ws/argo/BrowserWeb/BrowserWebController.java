8c8098d98a46b2bf4ab68a4eaeca1b118718f257
Jeff Simpson <jeff.simpson@volant-associates.com>
2015-04-30 13:53:20 -0400

    Now looks for argoClient.prop in external config file.  Handles sending more than 1 probe with a different respondTo address.  Full logging support.

--- BrowserWeb/src/main/java/ws/argo/BrowserWeb/BrowserWebController.java
+++ BrowserWeb/src/main/java/ws/argo/BrowserWeb/BrowserWebController.java
@@ -23,13 +23,17 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -44,10 +48,17 @@ import ws.argo.ProbeGenerator.Probe;
 import ws.argo.ProbeGenerator.ProbeGenerator;
 
 
+/*
+ * @author Jeff Simpson
+ * @since v0.0.1
+ */
+
 @Path("controller")
 public class BrowserWebController {
 	
-	private static final String PROBE_GENERATOR_PROPERTIES_FILE = "probeGenerator.props";
+	private final static Logger LOGGER = Logger.getLogger(BrowserWebController.class.getName());
+
+	private static final String PROBE_GENERATOR_PROPERTIES_FILE = "argoClient.prop";
 	private static final String DEFAULT_PROBE_RESPONSE_URL_PATH = "/AsynchListener/api/responseHandler/probeResponse";
 	private static final String DEFAULT_RESPONSES_URL_PATH = "/AsynchListener/api/responseHandler/responses";
 	private static final String DEFAULT_CLEAR_CACHE_URL_PATH = "/AsynchListener/api/responseHandler/clearCache";
@@ -57,7 +68,9 @@ public class BrowserWebController {
 	private static final String ARGO_HOME_PROPS_PATH = "/config/";
 	
 	protected CloseableHttpClient httpClient = null;
-	protected static Properties pgProps = null;
+	protected static Properties clientProps = null;
+	
+	protected static ArrayList<ProbeRespondToAddress> respondToAddresses = new ArrayList<ProbeRespondToAddress>();
 
 	
 	@GET
@@ -71,26 +84,32 @@ public class BrowserWebController {
 	@Produces("application/txt")
 	public String launchProbe() throws IOException {
 		
-		Properties pgProps = getPropeGeneratorProps();
+		Properties clientProps = getPropeGeneratorProps();
 		
-		String multicastGroupAddr = pgProps.getProperty("multicastGroupAddr", DEFAULT_MULTICAST_GROUP_ADDR);
-		String multicastPortString = pgProps.getProperty("multicastPort", DEFAULT_MULTICAST_PORT.toString());
-		String listenerIPAddress = pgProps.getProperty("listenerIPAddress");
-		String listenerPort = pgProps.getProperty("listenerPort");
-		String listenerURLPath = pgProps.getProperty("listenerProbeResponseURLPath", DEFAULT_PROBE_RESPONSE_URL_PATH);
+		String multicastGroupAddr = clientProps.getProperty("multicastGroupAddr", DEFAULT_MULTICAST_GROUP_ADDR);
+		String multicastPortString = clientProps.getProperty("multicastPort", DEFAULT_MULTICAST_PORT.toString());
+		
+//		String listenerIPAddress = clientProps.getProperty("listenerIPAddress");
+//		String listenerPort = clientProps.getProperty("listenerPort");
+		String listenerURLPath = clientProps.getProperty("listenerProbeResponseURLPath", DEFAULT_PROBE_RESPONSE_URL_PATH);
 		
 		Integer multicastPort = Integer.parseInt(multicastPortString);
 		
 		ProbeGenerator gen = new ProbeGenerator(multicastGroupAddr, multicastPort);
+
+		// loop over the "respond to addresses" specified in the properties file.
+		for (ProbeRespondToAddress rta : respondToAddresses) {
+			
+			Probe probe = new Probe("http://"+rta.respondToAddress+":"+rta.respondToPort+listenerURLPath, Probe.JSON);
+			// The following is a "naked" probe - no service contract IDs, etc.
+			// No specified service contract IDs implies "all"
+			// This will evoke responses from all reachable responders except those configured to "noBrowser"
+			gen.sendProbe(probe);
+		}
 		
-		Probe probe = new Probe("http://"+listenerIPAddress+":"+listenerPort+listenerURLPath, Probe.JSON);
-		
-		// No specified service contract IDs implies "all"
-		
-		gen.sendProbe(probe);
 		gen.close();
 		
-		return "Probe launched successfully on "+multicastGroupAddr+":"+ multicastPort;
+		return "Launched "+respondToAddresses.size()+" probe(s) successfully on "+multicastGroupAddr+":"+ multicastPort;
 
 	}
 	
@@ -98,11 +117,11 @@ public class BrowserWebController {
 	@Path("/responses")
 	@Produces("application/json")
 	public String getResponses() throws UnknownHostException {
-		Properties pgProps = getPropeGeneratorProps();
+		Properties clientProps = getPropeGeneratorProps();
 		
-		String listenerIPAddress = pgProps.getProperty("listenerIPAddress");
-		String listenerPort = pgProps.getProperty("listenerPort");
-		String listenerReponsesURLPath = pgProps.getProperty("listenerReponsesURLPath", DEFAULT_RESPONSES_URL_PATH);
+		String listenerIPAddress = clientProps.getProperty("listenerIPAddress");
+		String listenerPort = clientProps.getProperty("listenerPort");
+		String listenerReponsesURLPath = clientProps.getProperty("listenerReponsesURLPath", DEFAULT_RESPONSES_URL_PATH);
 		
 		String response = restGetCall("http://"+listenerIPAddress+":"+listenerPort+listenerReponsesURLPath);
 		
@@ -113,11 +132,11 @@ public class BrowserWebController {
 	@Path("/clearCache")
 	@Produces("application/json")
 	public String clearCache() throws UnknownHostException {
-		Properties pgProps = getPropeGeneratorProps();
+		Properties clientProps = getPropeGeneratorProps();
 		
-		String listenerIPAddress = pgProps.getProperty("listenerIPAddress");
-		String listenerPort = pgProps.getProperty("listenerPort");
-		String listenerClearCacheURLPath = pgProps.getProperty("listenerClearCacheURLPath", DEFAULT_CLEAR_CACHE_URL_PATH);
+		String listenerIPAddress = clientProps.getProperty("listenerIPAddress");
+		String listenerPort = clientProps.getProperty("listenerPort");
+		String listenerClearCacheURLPath = clientProps.getProperty("listenerClearCacheURLPath", DEFAULT_CLEAR_CACHE_URL_PATH);
 		
 		String response = restGetCall("http://"+listenerIPAddress+":"+listenerPort+listenerClearCacheURLPath);
 		
@@ -125,7 +144,8 @@ public class BrowserWebController {
 	}
 	
 	
-	private InputStream getPropertiesFileInputStream() {
+	@SuppressWarnings("resource")
+	private static InputStream getPropertiesFileInputStream() {
 		InputStream is = null;
 		
 		String ARGO_HOME = System.getenv(ARGO_HOME_ENV_NAME);
@@ -134,71 +154,131 @@ public class BrowserWebController {
 		File file = new File(externPropsFilename);
 		try {
 			is = new FileInputStream(file);
-			
 		} catch (FileNotFoundException e) {
-			System.out.println("External config file "+externPropsFilename+" not found.");
-			System.out.println("Using defaults located in classpath embedded in war file.");
-			is = this.getClass().getClassLoader().getResourceAsStream(PROBE_GENERATOR_PROPERTIES_FILE);
+			LOGGER.warning("External config file "+externPropsFilename+" not found.");
+			LOGGER.warning("Using defaults located in classpath embedded in war file.");
+			is = BrowserWebController.class.getClassLoader().getResourceAsStream(PROBE_GENERATOR_PROPERTIES_FILE);
 		}
 		
 		return is;
 	}
 	
-	private Properties getPropeGeneratorProps() throws UnknownHostException {
+	/*
+	 * provides the correct host IP address from a string (propIPAddress).
+	 * This method takes the IP addresses from the properties file and then resolves it to be
+	 * the correct IP address for the local host to use.
+	 * 
+	 * The propIPAddress might be empty.  In this case return the correct IP address for the local host
+	 * and not the loopback address.  This makes an assumption that if the user omitted the IP address
+	 * from the config file, then just use the local host address.
+	 * 
+	 * This handles the majority of cases where you don't know the IP address but want simple configuration.
+	 * It's a good way to default to the local host IP address.
+	 * 
+	 * @param	propIPAddress the IP address from the properties file
+	 * 
+	 * @return	the value of the correct string for the local host IP address if the propIPAddress is empty
+	 */
+	private static String getHostIPAddressFromString(String propIPAddress, String niName) {
+		String hostIPAddr = propIPAddress;
+
+		if (propIPAddress.equals("")) {
+			// If the listenerIPAddress is blank, then try to get the ip address of the interface
+			
+			try {
+				NetworkInterface ni = null;
+				if (niName != null)
+					ni = NetworkInterface.getByName(niName);
+				if (ni == null) {
+					LOGGER.info("Network Interface name not specified or incorrect.  Defaulting to localhost");
+					hostIPAddr = InetAddress.getLocalHost().getHostAddress();
+				} else {
+				
+					if (!ni.isLoopback()) {
+					    
+					    Enumeration ee = ni.getInetAddresses();
+					    while (ee.hasMoreElements())
+					    {
+					        InetAddress i = (InetAddress) ee.nextElement();
+					        if (i instanceof Inet4Address) {
+					        	hostIPAddr = i.getHostAddress();
+					        	break;  // get the first one an get out of the loop
+					        }
+					    }
+					}
+				}
+			
+			} catch (SocketException e) {
+				LOGGER.warning("A socket exception occurred.");
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				LOGGER.log(Level.WARNING, "Error finding Network Interface", e);
+			}
+			
+		}		
 		
-		if (pgProps != null)
-			return pgProps;
+		return hostIPAddr;
+	}
+	
+	private static synchronized Properties getPropeGeneratorProps() throws UnknownHostException {
+		
+		if (clientProps != null)
+			return clientProps;
 		
 		InputStream in = getPropertiesFileInputStream();
 		
-		pgProps = new Properties();
+		clientProps = new Properties();
 		
 		if (in != null) {
 			try {
-				pgProps.load(in);
+				clientProps.load(in);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				// Should log the props file issue
-				setDefaultProbeGeneratorProperties(pgProps);
+				setDefaultProbeGeneratorProperties(clientProps);
 			}
 			
 		} 
 		
+		String niName = clientProps.getProperty("networkInterfaceName");
+		if (niName.isEmpty()) niName = null;
 		
+		// Get the listener IP address (for cache related GUI calls) - usually localhost
 		
-		if (pgProps.getProperty("listenerIPAddress","").equals("")) {
-			// If the listenerIPAddress is blank, then try to get the ip address of the interface
-			
-			String hostIPAddr = null;
-			
-			try {
-				NetworkInterface n = NetworkInterface.getByName(pgProps.getProperty("listenerInterfaceName","en0"));
-
-				if (!n.isLoopback()) {
-				    
-				    Enumeration ee = n.getInetAddresses();
-				    while (ee.hasMoreElements())
-				    {
-				        InetAddress i = (InetAddress) ee.nextElement();
-				        hostIPAddr = i.getHostName();
-				        System.out.println(hostIPAddr);
-				    }
-				}
-			} catch (SocketException e) {
-				System.out.println("A socket exception occurred.");
-			}
-			
-			if (hostIPAddr == null) {
-				hostIPAddr = InetAddress.getLocalHost().getHostName();
-				System.out.println("Defaulting to local address: "+hostIPAddr);
-			}
-			pgProps.put("listenerIPAddress", hostIPAddr);
+		String listenerIPAddress = clientProps.getProperty("listenerIPAddress","");
+		if (listenerIPAddress.equals("")) {
+			String hostIPAddr = getHostIPAddressFromString(listenerIPAddress, niName);
+			clientProps.put("listenerIPAddress", hostIPAddr);
 		}
 		
-		return pgProps;
+		// handle the list of appHandler information
+		
+		boolean continueProcessing = true;
+		int number = 1;
+		while (continueProcessing) {
+			String respondToAddress;
+			String respondToPort;	
+			
+			respondToAddress = clientProps.getProperty("respondToIPAddress."+number,"");
+			respondToPort = clientProps.getProperty("respondToPort."+number, "80");
+			
+			if (clientProps.containsKey("respondToIPAddress."+number)) {
+				String hostIPAddr = getHostIPAddressFromString(respondToAddress, niName);
+				ProbeRespondToAddress rta = new ProbeRespondToAddress();
+				rta.respondToAddress = hostIPAddr;
+				rta.respondToPort = Integer.valueOf(respondToPort);
+				respondToAddresses.add(rta);
+			} else {
+				continueProcessing = false;
+			}
+			number++;
+			
+		}		
+		
+		return clientProps;
 	}
 	
-	private void setDefaultProbeGeneratorProperties(Properties props) {
+	private static void setDefaultProbeGeneratorProperties(Properties props) {
 		props.put("multicastPort", DEFAULT_MULTICAST_PORT);
 		props.put("multicastGroupAddr", DEFAULT_MULTICAST_GROUP_ADDR);
 		props.put("listenerIPAddress", "");
@@ -228,7 +308,6 @@ public class BrowserWebController {
 					StringBuffer buf = new StringBuffer();
 	
 					String output;
-					//System.out.println("Output from Listener .... \n");
 					while ((output = br.readLine()) != null) {
 						buf.append(output);
 					}
@@ -239,17 +318,13 @@ public class BrowserWebController {
 				httpClient.close();
 			}
 			
-//			System.out.println("Response payload sent successfully to respondTo address.");
 
 		} catch (MalformedURLException e) {
-			System.out.println("MalformedURLException occured\nThe respondTo URL was a no good.  respondTo URL is: "+url);
-//			e.printStackTrace();
+			LOGGER.log(Level.SEVERE, "nThe respondTo URL was a no good.  respondTo URL is: "+url, e);
 		} catch (IOException e) {
-			System.out.println("An IOException occured: the error message is - "+e.getMessage());
-//			e.printStackTrace();
+			LOGGER.log(Level.SEVERE, "An IOException occured: the error message is - ", e);
 		} catch (Exception e) {
-			System.out.println("Some error occured: the error message is - "+e.getMessage());
-//			e.printStackTrace();
+			LOGGER.log(Level.SEVERE, "Some error occured: the error message is - ", e);
 		}
 		
 		return response;
@@ -264,6 +339,5 @@ public class BrowserWebController {
 		return httpClient;
 		
 	}
-
 	
 }
