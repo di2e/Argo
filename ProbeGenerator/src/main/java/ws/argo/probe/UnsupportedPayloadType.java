79cd435ca14d99797fe757617c842e8cfd055c9e
Jeff Simpson <jeff.simpson@volant-associates.com>
2015-06-18 09:33:40 -0400

    integrated refactored wireline format and tested with unit testing

--- ProbeGenerator/src/main/java/ws/argo/probe/UnsupportedPayloadType.java
+++ ProbeGenerator/src/main/java/ws/argo/probe/UnsupportedPayloadType.java
@@ -2,19 +2,14 @@ package ws.argo.probe;
 
 public class UnsupportedPayloadType extends Exception {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 346457292691086877L;
+  private static final long serialVersionUID = 346457292691086877L;
 
-	public UnsupportedPayloadType() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	public UnsupportedPayloadType(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-	
+  public UnsupportedPayloadType() {
+    super();
+  }
+
+  public UnsupportedPayloadType(String message) {
+    super(message);
+  }
+
 }
